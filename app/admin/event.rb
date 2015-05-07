47e4b4603d7a154f473296d8366b72fdcd485210
morinpic <morinpic0518@gmail.com>
2015-05-07 20:48:48 +0900

    - modify ActiveAdminのEventモデルの詳細ページにタグリストを表示

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Event do
   permit_params :title, :body, :tag_list, :event_date_date, :event_date_time_hour, :event_date_time_minute
+  config.per_page = 15 # 一覧ページのページングの件数
 
   index do
     selectable_column
@@ -9,6 +10,16 @@ ActiveAdmin.register Event do
     column :event_date
     column :tag_list
     actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :body
+      row :event_date
+      row :tag_list
+    end
   end
 
   form do |f|
